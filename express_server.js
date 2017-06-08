5beb0b09131c947a2e67a304989031d41bd6cd52
vagrant <zlymike@outlook.com>
2017-06-08 00:24:24 +0000

    finished login logout

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 8080;
 const bodyParser =require("body-parser");
+var cookieParser = require('cookie-parser')
+app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -21,19 +23,24 @@ function generateRandomString() {
 }
 
 app.get("/urls", (req, res) => {
-  let templateVars = {urls: urlDatabase};
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+
   res.render("urls_index", templateVars);
 });
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", {username: req.cookies['username']});
 });
 
 
 app.get("/urls/:id", (req, res) => {
   let shortURL = req.params.id
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: shortURL,
     longURL: urlDatabase[shortURL]
   };
@@ -54,7 +61,16 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username)
+  console.log("Cookie set")
+  res.redirect('/urls');
+})
 
+app.post('/logout', (req, res) => {
+  res.clearCookie('username')
+  res.redirect('/urls');
+})
 app.post("/urls", (req, res) => {
   console.log(req.body);
   var randomGen = generateRandomString();
